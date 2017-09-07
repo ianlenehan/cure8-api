c4bc8ed354bf2e78f9413d28b5fef2fc37da60a3
Ian Lenehan <ianlenehan@gmail.com>
2017-09-07 21:53:27 +1000

    hopefully fix notifications

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -101,15 +101,15 @@ module Api::V1
         group_ids.each do |group_id|
           group = Group.find(group_id)
           if group.user_id
-            user = User.find(group.user_id)
-            new_link_notification(user, link)
+            recipient = User.find(group.user_id)
+            new_link_notification(recipient, link)
             Curation.create(user_id: group.user_id, link_id: link.id, comment: comment)
             send_sms(group.user_id, link.id)
           else
             group.members.each do |member_id|
               user_group = Group.find(member_id)
-              user = User.find(user_group.user_id)
-              new_link_notification(user, link)
+              recipient = User.find(user_group.user_id)
+              new_link_notification(recipient, link)
               Curation.create(user_id: user_group.user_id, link_id: link.id, comment: comment)
               send_sms(user_group.user_id, link.id)
             end
@@ -125,7 +125,7 @@ module Api::V1
       if !user.first_name
         @client = Twilio::REST::Client.new twilio[:account_sid], twilio[:auth_token]
         message = @client.account.messages.create(
-        :body => "Your friend #{curator.name} has saved a link for you on cure8. This can be accessed by first downloading 'expo client' from the App Store or Google Play Store. Once installed, open this link: https://exp.host/@ianlenehan/cure8-app",
+        :body => "Your friend #{curator.name} has saved a link for you on cure8. Download the Cure8 app from the App Store or Google Play Store to view the link and start curating content for your friends!",
         :to => user.phone,
         # :from => "+15005550006"
         :from => "+61429806720"
@@ -133,15 +133,14 @@ module Api::V1
       end
     end
 
-    def new_link_notification(user, link)
-      if user.notifications_new_link && user.push_token
-        curator = User.find(link.link_owner)
+    def new_link_notification(recipient, link)
+      if recipient.notifications_new_link && recipient.push_token
         details = {
           from: user.name,
           title: link.title,
           type: 'curation'
         }
-        send_notification(user, details)
+        send_notification(recipient, details)
       end
     end
 
@@ -159,9 +158,9 @@ module Api::V1
       end
     end
 
-    def send_notification(user, details)
-      if user.notifications
-        user.push_tokens.each do |push_token|
+    def send_notification(recipient, details)
+      if recipient.notifications
+        recipient.push_tokens.each do |push_token|
           push_notification.publish(push_token.token, details)
         end
       end
