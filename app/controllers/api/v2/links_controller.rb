3bf17f61e9c2ba0c9b00b5235c047a46c587c9ff
Ian Lenehan <ian.lenehan@blake.com.au>
2018-06-10 22:18:17 +1000

    proper controller this time - v2

--- app/controllers/api/v2/links_controller.rb
+++ app/controllers/api/v2/links_controller.rb
@@ -144,7 +144,7 @@ module Api::V2
           else
             group.members.each do |member_id|
               user_group = Group.find(member_id)
-              create_for_group(user_group)
+              create_for_group(user_group, comment, link)
             end
           end
         end
