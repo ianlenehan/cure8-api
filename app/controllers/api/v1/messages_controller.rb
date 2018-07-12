0b8e97815bf8518562f532771878e57e1d72b3bc
Ian Lenehan <ian.lenehan@blake.com.au>
2018-07-12 22:31:05 +1000

    Take token when creating message

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,11 @@
 module Api::V1
   class MessagesController < ApplicationController
     def create
-      message = Message.new(message_params)
+      message = Message.new({
+        text: message_params[:text],
+        conversation_id: message_params[:conversation_id],
+        user_id: app_user.id
+        })
       conversation = Conversation.find(message_params[:conversation_id])
       if message.save
         serialized_data = ActiveModelSerializers::Adapter::Json.new(
@@ -15,7 +19,19 @@ module Api::V1
     private
 
     def message_params
-      params.require(:message).permit(:text, :conversation_id, :user_id)
+      params.require(:message).permit(:text, :conversation_id)
+    end
+
+    def user_params
+      params.require(:user).permit(:token)
+    end
+
+    def app_user
+      db_token.user
+    end
+
+    def db_token
+      Token.find_by(token: user_params[:token])
     end
   end
 end
