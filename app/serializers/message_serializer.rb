fb630862039dccbb49ca4c607ed60182894107ba
Ian Lenehan <ian.lenehan@blake.com.au>
2018-07-17 23:54:42 +1000

    can I serialize conversation

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,9 +1,13 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :conversation_id, :text, :created_at, :user_id, :user
+  attributes :id, :conversation_id, :text, :created_at, :user_id, :user, :conversation
 
   def user
     u = User.find(object.user_id)
 
     { _id: object.user_id, name: u.name }
+  end
+
+  def conversation
+    object.conversation
   end
 end
